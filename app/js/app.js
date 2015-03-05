313ef9ed517d56348136f0e5b728d3022f94f101
katyf <katy.feng.14@gmail.com>
2015-03-05 16:04:18 -0500

    js for getting and rendering movies and creating new movies

--- app/js/app.js
+++ app/js/app.js
@@ -9,6 +9,7 @@ var trace = function(){
 var App = App || {
   url: 'http://localhost:3000'
 };
+
 App.getMovies = function(){
   $.ajax({
     url: App.url + '/movies',
@@ -39,8 +40,10 @@ App.submitMovie = function(){
     type: 'POST',
     data: { movie: {
       title: $('input#movie-title').val(),
-      body: $('textarea#movie-body').val(),
-      author: $('input#movie-author').val()
+      total_gross: $('textarea#movie-gross').val(),
+      release_date: $('input#movie-release').val(),
+      MPAA_rating: $('input#movie-rating').val(),
+      description: $('input#movie-description').val(),
       }
     },
   })
@@ -56,42 +59,42 @@ App.submitMovie = function(){
 };
 
 
-App.editMovie = function(movie) {
-  var movieId = parseInt(movie);
-  var $newTitle, $newBody;
-  var $title = $('#'+ movie + '.movie h3').text();
-  var $body = $('#'+ movie + '.movie :not(h3)').text();
-  var $movie = $('#'+ movie + '.movie');
-  var originalHTML = $movie.html();
+// App.editMovie = function(movie) {
+//   var movieId = parseInt(movie);
+//   var $newTitle, $newBody;
+//   var $title = $('#'+ movie + '.movie h3').text();
+//   var $body = $('#'+ movie + '.movie :not(h3)').text();
+//   var $movie = $('#'+ movie + '.movie');
+//   var originalHTML = $movie.html();
 
-  $movie.html('<div class="movie-form"><form id="edit-movie-form"><div class="form-group"><input name="movie-title" type="text" value="'+ $title +'" id=' + movieId + ' class="clear-me" /></div><div class="form-group"><label for="movie-body">movie Body</label><textarea name="movie-body"  id="movie-body"></textarea></div><div class="form-group"><input type="button" id="save" value="Save movie" /><input type="button" id="cancel" value="Cancel" /></div></form></div>');
-  $('.movie#' + movie + ' [name=movie-body]').val($body);
+//   $movie.html('<div class="movie-form"><form id="edit-movie-form"><div class="form-group"><input name="movie-title" type="text" value="'+ $title +'" id=' + movieId + ' class="clear-me" /></div><div class="form-group"><label for="movie-body">movie Body</label><textarea name="movie-body"  id="movie-body"></textarea></div><div class="form-group"><input type="button" id="save" value="Save movie" /><input type="button" id="cancel" value="Cancel" /></div></form></div>');
+//   $('.movie#' + movie + ' [name=movie-body]').val($body);
 
-  var $saveButton = $('#edit-†save');
-      $saveButton.on('click', function(){
-        $newTitle = $('[name=edit-movie-title]').val();
-        $newBody = $('[name=edit-movie-body]').val();
-        $.ajax({
-          url: App.url + '/movies/' + movieId,
-          type: 'PATCH',
-          data: {
-            movie: {
-              title: $newTitle,
-              body: $newBody,
-            },
-          },
-        }).done(function(data){
-          trace(data);
+//   var $saveButton = $('#edit-†save');
+//       $saveButton.on('click', function(){
+//         $newTitle = $('[name=edit-movie-title]').val();
+//         $newBody = $('[name=edit-movie-body]').val();
+//         $.ajax({
+//           url: App.url + '/movies/' + movieId,
+//           type: 'PATCH',
+//           data: {
+//             movie: {
+//               title: $newTitle,
+//               body: $newBody,
+//             },
+//           },
+//         }).done(function(data){
+//           trace(data);
 
-          $movie.html('<h3>' + $newTitle + '</h3>' + '<p>' + $newBody + '</p>' + '<input type=button class=edit id=' + movieId + ' value="Edit movie" >' + '<input type=button class=delete id=' + movieId + ' value="Delete movie" >');
+//           $movie.html('<h3>' + $newTitle + '</h3>' + '<p>' + $newBody + '</p>' + '<input type=button class=edit id=' + movieId + ' value="Edit movie" >' + '<input type=button class=delete id=' + movieId + ' value="Delete movie" >');
 
-          App.setMovieButtonHandlers();
-        })
-        .fail(function(jqXHR, textStatus, errorThrown){
-          trace(jqXHR, textStatus, errorThrown);
-        });
-      });
-};
+//           App.setMovieButtonHandlers();
+//         })
+//         .fail(function(jqXHR, textStatus, errorThrown){
+//           trace(jqXHR, textStatus, errorThrown);
+//         });
+//       });
+// };
 
 $(document).ready(function(){
 
