3b075b870948824a42702c3d806719ce53b2952f
Daniel Tyson <danielctyson@gmail.com>
2015-03-06 10:25:38 -0500

    namespaced review functions

--- app/js/app.js
+++ app/js/app.js
@@ -34,6 +34,10 @@ App.renderMovie = function(movie, index, array) {
   App.getReviews(movie);
 };
 
+// App.renderNewReview = function(movie) {
+//       '<section class="review-form"><header><h1>Add A Review</h1></header><form class="new-review-form"><div class="form-group"><label for="review-author">Review Author</label><br><input type="text" id="review-author" class="clearme" /></div><div class="form-group">        <label for="review-body">Review</label><br>        <textarea id="review-body" class="clearme"></textarea>    </div>    <div class="form-group">        <label for="review-rating">Rating</label><br>        1<input type="radio" name="rating" value="1">        2<input type="radio" name="rating" value="2">        3<input type="radio" name="rating" value="3">        4<input type="radio" name="rating" value="4">        5<input type="radio" name="rating" value="5">    </div>    <div class="form-group">        <input type="submit" value="create review" class="btn" />    </div></form>'
+// }
+
 App.submitMovie = function(){
   if(event.preventDefault) event.preventDefault();
   $.ajax({
@@ -78,9 +82,29 @@ App.renderReview = function(review, index, array) {
   $('#'+ this.title.replace(/(\s)+/g, '') +' .reviews').append('<li class="review">' + '<p class="review-body">' + review.body + '</p>' + '<p class="review-rating">' + review.rating + '</p>' + '<p class="review-author">' + review.author + '</p>' +'</li>');
 };
 
+App.submitReview = function(){
+  $.ajax({
+    url: App.url + '/movies/' + movie.id + '/reviews',
+    type: 'get',
+    data: { review: {
+      author: $('insert author box here').val(),
+      body: $('insert body box here').val(),
+      rating: $('insert rating such here').val()
+      }
+    },
+
+  })
+  .done(function(data) {
+    trace(data);
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    trace(jqXHR, textStatus, errorThrown);
+  });
+};
+
 // App.deleteReview = function(review){
 //   $.ajax({
-//     url: App.url + '/admin/reviews' + review.id.
+//     url: App.url + '/admin/reviews' + review.id,
 //     type: 'DELETE'
 //   })
 //   .done(function(data) {
@@ -93,26 +117,6 @@ App.renderReview = function(review, index, array) {
 
 
 
-
-// App.submitReview = function(){
-//   $.ajax({
-//     url: App.url + '/movies/' + movie.id + '/reviews',
-//     type: 'get',
-//     data: { review: {
-//       author: $('insert author box here').val(),
-//       body: $('insert body box here').val(),
-//       rating: $('insert rating such here').val()
-//       }
-//     },
-
-//   })
-//   .done(function(data) {
-//     trace(data);
-//   })
-//   .fail(function(jqXHR, textStatus, errorThrown) {
-//     trace(jqXHR, textStatus, errorThrown);
-//   });
-// };
 
 
 
